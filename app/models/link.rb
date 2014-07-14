3561000155ad6e336d807ecd0f3b92afc201ffda
wubwubwubwub <nolan.camp@gmail.com>
2014-07-14 08:36:47 -0400

    added vote model, set relationships to link/comment

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  has_many :votes
   #attr_accessor :url, :title <-- this is used primarily for anything that is NOT a database column
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
-class User < ActiveRecord::Base
-  
+class User < ActiveRecord::Base 
   has_many :links
   has_many :comments
+  has_many :votes
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         
+         
+         
 end
